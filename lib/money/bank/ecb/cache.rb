40242e1a40f68220c8df06416976c2db02cb320a
Casper Thomsen <ct@clearhaus.com>
2014-02-08 10:22:23 +0100

    Clean-up and add documentation.

--- lib/money/bank/ecb/cache.rb
+++ lib/money/bank/ecb/cache.rb
@@ -2,20 +2,25 @@ class Money
   module Bank
     class ECB < Money::Bank::VariableExchange
       module Cache
-        def initialize(*); end
-        def self.new_from?(*); false end
-        def self.priority; -1 end
-
-        def set(&block); end
-        def get; end
-
         @@classes = []
+        # Use callback to store what class is including Cache.
         def self.included(cache_class)
           @@classes << cache_class
         end
 
-        def self.new_from?(_)
-          raise StandardError, "Implement '.new_from?(arg)' in your Cache!"
+        # Array of the kind of Caches that are around.
+        #
+        # @return [Array<Class>]
+        def self.classes
+          @@classes
+        end
+
+        # Tells if the arguments can be used to build such a Cache.
+        #
+        # @param [*]
+        # @return [true,false]
+        def self.new_from?(*)
+          raise StandardError, "Implement '.new_from?(*args)' in your Cache!"
         end
 
         # Builds a Cache from some input.
@@ -23,10 +28,28 @@ class Money
         # @param [*] something The thing to build a Cache from.
         # @return [Cache]
         def self.build(something)
-          @@classes.select{|klass| klass.new_from?(something)}
+          Cache.classes.select{|klass| klass.new_from?(something)}
             .sort_by{|klass| -klass.priority}
             .first
             .new(something)
+        end
+
+        def initialize(*)
+        end
+
+        # Set the cache to whatever the block yields.
+        #
+        # @yield [] block
+        # @yieldreturn [String]
+        def set(&block)
+          raise StandardError, "Implement '.set(&block)' in your Cache!"
+        end
+
+        # Get what the cache contains.
+        #
+        # @return [String]
+        def get
+          raise StandardError, "Implement '.get' in your Cache!"
         end
       end
     end
